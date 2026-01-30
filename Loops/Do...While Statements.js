eb79b7cee49676795c647b957183c6fdac6f5ab5
ShahoRasooli <schaho.rasooli@gmail.com>
2026-01-30 11:23:58 +0000

    Do...While Statements

---
+++ Loops/Do...While Statements.js
@@ -0,0 +1,34 @@
+let countString = "";
+let i = 0;
+
+do {
+  countString = countString + i;
+  i++;
+} while (i < 5);
+
+console.log(countString);
+
+/////////////////
+
+const firstMessage = "I will print!";
+const secondMessage = "I will not print!";
+
+// A do while with a stopping condition that evaluates to false
+do {
+  console.log(firstMessage);
+} while (true === false);
+
+// A while loop with a stopping condition that evaluates to false
+while (true === false) {
+  console.log(secondMessage);
+}
+
+/////////////////
+
+const cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
+
+do {
+  cupsAdded++;
+  console.log(cupsAdded + " cup was added");
+} while (cupsAdded < cupsOfSugarNeeded);
