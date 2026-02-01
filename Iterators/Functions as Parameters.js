2b0ac04464822acc90844f58eb6eba581e72616a
ShahoRasooli <schaho.rasooli@gmail.com>
2026-02-01 12:42:50 +0000

    Functions as Parameters

---
+++ Iterators/Functions as Parameters.js
@@ -0,0 +1,29 @@
+const higherOrderFunc = (param) => {
+  param();
+  return `I just invoked ${param.name} as a callback function!`;
+};
+
+const anotherFunc = () => {
+  return "I'm being invoked by the higher-order function!";
+};
+
+higherOrderFunc(anotherFunc);
+
+//////////////////
+
+const addTwo = (num) => {
+  return num + 2;
+};
+
+const checkConsistentOutput = (func, val) => {
+  const checkA = val + 2;
+  const checkB = func(val);
+
+  if (checkA === checkB) {
+    return func(val);
+  } else {
+    return "inconsistent results";
+  }
+};
+
+console.log(checkConsistentOutput(addTwo, 5));
