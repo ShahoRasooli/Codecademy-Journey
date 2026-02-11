2ab1127c0ae20cb35838e656561041d41786e314
ShahoRasooli <schaho.rasooli@gmail.com>
2026-02-11 14:57:20 +0000

    Arrow Functions and this

---
+++ Objects/Arrow Functions and this.js
@@ -0,0 +1,23 @@
+const goat = {
+  dietType: "herbivore",
+  makeSound() {
+    console.log("baaa");
+  },
+  diet: () => {
+    console.log(this.dietType);
+  },
+};
+
+goat.diet(); // Prints undefined
+
+/////////////////////////////
+
+const robot = {
+  energyLevel: 100,
+  checkEnergy() {
+    console.log(`Energy is currently at ${this.energyLevel}%.`);
+  },
+};
+
+robot.checkEnergy();
+// Energy is currently at 100%.
